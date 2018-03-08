7587e47ee7ca5469b406e000c1732f8cff8f4097
Valerian Gaudeau <valerian.gaudeau@gmail.com>
2018-03-08 21:50:25 +0100

    improved library load path

--- emacs/init.d/init-libraries.el
+++ emacs/init.d/init-libraries.el
@@ -1,4 +1,4 @@
 ;; load path
-(add-to-list 'load-path "~/.emacs.d/libraries/paradox/")
+(add-to-list 'load-path (expand-file-name "../libraries/paradox/" (file-name-directory load-file-name)))
 
 (provide 'init-libraries)
