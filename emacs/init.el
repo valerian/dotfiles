0138810f019e9a006ced925ae78e3d543ad922bb
Valerian Gaudeau <valerian.gaudeau@gmail.com>
2018-12-09 12:48:11 +0100

    removed unnecessary package source that caused duplicate packages issue

--- emacs/init.el
+++ emacs/init.el
@@ -8,9 +8,6 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 ;(require 'benchmark-init)
